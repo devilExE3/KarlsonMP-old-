bc1875ca25a6cc1f029911e3d434608aa5bdaec8
TSBYT <devilexe1337@gmail.com>
2021-04-29 00:09:35 +0300

    generic thing

--- KarlsonMPmelon/ClassLibrary1/ClassLibrary1/HarmonyHooks.cs
+++ KarlsonMPmelon/ClassLibrary1/ClassLibrary1/HarmonyHooks.cs
@@ -5,7 +5,7 @@ using HarmonyLib;
 
 namespace KarlsonMP
 {
-    [HarmonyPatch(typeof(Enemy), "LateUpdate")]
+    /*[HarmonyPatch(typeof(Enemy), "LateUpdate")]
     public static class Enemy_LateUpdate
     {
         public static bool Prefix(Enemy __instance)
@@ -20,6 +20,42 @@ namespace KarlsonMP
     public static class Debug_Update
     {
         public static bool Prefix(Debug __instance)
+        {
+            __instance.GetType().GetMethod("Fps").Invoke(__instance, Array.Empty<object>());
+            if (Input.GetKeyDown(KeyCode.Tilde))
+            {
+                if (__instance.console.isActiveAndEnabled)
+                    __instance.GetType().GetMethod("CloseConsole").Invoke(__instance, Array.Empty<object>());
+                else
+                    __instance.GetType().GetMethod("OpenConsole").Invoke(__instance, Array.Empty<object>());
+            }
+            return false;
+        }
+    }*/
+
+    [HarmonyPatch]
+    class Enemy_LateUpdate
+    {
+        static System.Reflection.MethodBase TargetMethod()
+        {
+            return typeof(Enemy).GetMethod("LateUpdate").MakeGenericMethod();
+        }
+        static bool Prefix(Enemy __instance)
+        {
+            if (__instance.IsDead())
+                return false;
+            return __instance.GetComponent<NavMeshAgent>().enabled; // LateUpdate cancels animations
+        }
+    }
+
+    [HarmonyPatch]
+    class Debug_Update
+    {
+        static System.Reflection.MethodBase TargetMethod()
+        {
+            return typeof(Debug).GetMethod("Update").MakeGenericMethod();
+        }
+        static bool Prefix(Debug __instance)
         {
             __instance.GetType().GetMethod("Fps").Invoke(__instance, Array.Empty<object>());
             if (Input.GetKeyDown(KeyCode.Tilde))
