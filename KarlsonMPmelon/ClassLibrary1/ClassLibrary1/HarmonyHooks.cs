d0166a5350275d4827e335badc1b6c9021d3fe90
TSBYT <devilexe1337@gmail.com>
2021-04-28 23:58:52 +0300

    WTF #2

--- KarlsonMPmelon/ClassLibrary1/ClassLibrary1/HarmonyHooks.cs
+++ KarlsonMPmelon/ClassLibrary1/ClassLibrary1/HarmonyHooks.cs
@@ -5,34 +5,31 @@ using HarmonyLib;
 
 namespace KarlsonMP
 {
-    class HarmonyHooks
+    [HarmonyPatch(typeof(Enemy), "LateUpdate")]
+    public class Enemy_LateUpdate
     {
-        [HarmonyPatch(typeof(Enemy), "LateUpdate")]
-        public class Enemy_LateUpdate
+        public static bool Prefix(Enemy __instance)
         {
-            public static bool Prefix(Enemy __instance)
-            {
-                if (__instance.IsDead())
-                    return false;
-                return __instance.GetComponent<NavMeshAgent>().enabled; // LateUpdate cancels animations
-            }
-        }
-
-        [HarmonyPatch(typeof(Debug), "Update")]
-        public class Debug_Update
-        {
-            public static bool Prefix(Debug __instance)
-            {
-                __instance.GetType().GetMethod("Fps").Invoke(__instance, Array.Empty<object>());
-                if (Input.GetKeyDown(KeyCode.Tilde))
-                {
-                    if (__instance.console.isActiveAndEnabled)
-                        __instance.GetType().GetMethod("CloseConsole").Invoke(__instance, Array.Empty<object>());
-                    else
-                        __instance.GetType().GetMethod("OpenConsole").Invoke(__instance, Array.Empty<object>());
-                }
+            if (__instance.IsDead())
                 return false;
+            return __instance.GetComponent<NavMeshAgent>().enabled; // LateUpdate cancels animations
+        }
+    }
+
+    [HarmonyPatch(typeof(Debug), "Update")]
+    public class Debug_Update
+    {
+        public static bool Prefix(Debug __instance)
+        {
+            __instance.GetType().GetMethod("Fps").Invoke(__instance, Array.Empty<object>());
+            if (Input.GetKeyDown(KeyCode.Tilde))
+            {
+                if (__instance.console.isActiveAndEnabled)
+                    __instance.GetType().GetMethod("CloseConsole").Invoke(__instance, Array.Empty<object>());
+                else
+                    __instance.GetType().GetMethod("OpenConsole").Invoke(__instance, Array.Empty<object>());
             }
+            return false;
         }
     }
 }
